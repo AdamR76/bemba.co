c3d140a0d703555b38f6757ed57dcb380f567f6a
adam <adam@demandred.com>
2025-06-11 19:20:23 -0600

    styling

--- js/utils/utils.mjs
+++ js/utils/utils.mjs
@@ -19,27 +19,27 @@ const chunk = (arr, size) => {
 const onSubmit = (evt, fn) => {
 	evt.preventDefault();
 	const values = chunk(
-			querySelect('.taskform input, .taskform textarea, .taskform select')
-					.map(el => ({ [el.name]: el.value })), 7)
-			.map((el, idx) => Object.assign(...el, { weight: idx + 1, pid }));
+		querySelect('.taskform input, .taskform textarea, .taskform select')
+			.map(el => ({ [el.name]: el.value })), 7)
+		.map((el, idx) => Object.assign(...el, { weight: idx + 1, pid }));
 	console.log({ values, pid, creds })
 	flow(
-			ajax({ path: 'projects/updatetasks', data: { values, creds } }),
-			() => ajax({ path: 'projects/getproject', data: { token: creds.token, t, pid } }),
-			project => updateElement(container, fn(project))
+		ajax({ path: 'projects/updatetasks', data: { values, creds } }),
+		() => ajax({ path: 'projects/getproject', data: { token: creds.token, t, pid } }),
+		project => updateElement(container, fn(project))
 	)
 };
 
 
-const taskform = project => html('form', 
-	{ draggable: true, onsubmit: evt => onSubmit(evt, taskform), className: 'taskform' },
-	[drawMetrics(project), ...grouper(project).map(drawTasks), html('button', { type: 'submit', className: 'btn' }, 'Update Tasks')].flat(Infinity));
+const taskform = project => html('form',
+	{ draggable: true, onsubmit: evt => onSubmit(evt, taskform), onchange: evt => onSubmit(evt, taskform), className: 'taskform' },
+	[drawMetrics(project), ...grouper(project).map(drawTasks)].flat(Infinity));
 
 
 const drawTasks = projects => {
 	const [{ projectcat }] = projects;
 	return html('details', { open: true }, [html('summary', {}, projectcat), ...projects.map(project => html('div', {
-		className: 'flex row flexwrap task',
+		className: `flex row flexwrap task ${project.phaseid === 5 ? 'done' : ''}`,
 		draggable: true,
 		'data-projectitemid': project.projectitemid,
 		ondragstart: evt => {
@@ -63,32 +63,12 @@ const drawTasks = projects => {
 				}
 			}
 		},
-		ondrop: evt => evt.preventDefault(),
+		ondrop: evt => onSubmit(evt, taskform),
 	}, [
-		html('div', { className: 'flex column' }, [
+		html('div', { className: `flex column` }, [
 			html('label', {}, [
 				'Task Title',
 				html('input', { type: 'text', value: project.title, name: 'title' })
-			]),
-			html('label', {}, [
-				'Task Description',
-				html('textarea', { value: project.description, name: 'description' })
-			]),
-			html('div', { className: 'deleteContainer flex row around', 'data-project': project.projectitemid },
-				buttons(project))
-		]),
-		html('div', {}, [
-			html('label', {}, [
-				'Phase',
-				html('select', { name: 'phase', 'data-id': project?.projectitemid }, project.phases.map(phase =>
-					html('option', { value: phase.phaseid, selected: phase.phaseid === project.phaseid }, phase.phase)))
-			]),
-			html('label', {}, [
-				'Due',
-				html('input', {
-					type: 'date', defaultValue: project?.duedate?.split('T')[0],
-					name: 'duedate', 'data-id': project?.projectitemid
-				}),
 			]),
 			html('label', {}, [
 				'Assign To',
@@ -96,6 +76,27 @@ const drawTasks = projects => {
 					project.users.concat({ name: 'Not Assigned', userid: 0 })
 						.reverse().map(user =>
 							html('option', { value: user.userid, selected: user.userid === project.assignto }, user.name))),
+			]),
+			html('label', {}, [
+				'Phase',
+				html('select', { name: 'phase', 'data-id': project?.projectitemid }, project.phases.map(phase =>
+					html('option', { value: phase.phaseid, selected: phase.phaseid === project.phaseid }, phase.phase)))
+			]),
+
+			html('div', { className: 'deleteContainer flex row around', 'data-project': project.projectitemid },
+				buttons(project))
+		]),
+		html('div', { className: 'flex column' }, [
+			html('label', {}, [
+				'Task Description',
+				html('textarea', { value: project.description, name: 'description' })
+			]),
+			html('label', {}, [
+				'Due',
+				html('input', {
+					type: 'date', defaultValue: project?.duedate?.split('T')[0],
+					name: 'duedate', 'data-id': project?.projectitemid
+				}),
 			]),
 			html('input', { type: 'hidden', name: 'projectitemid', value: project.projectitemid }),
 			html('input', { type: 'hidden', name: 'projectcatid', value: project.projectcatid })
