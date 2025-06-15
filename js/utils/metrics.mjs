72da17d40ee16c1c0edaf7e079232485688eb675
adam <adam@demandred.com>
2025-06-15 09:27:32 -0600

    don't show metrics if there are no items

--- js/utils/metrics.mjs
+++ js/utils/metrics.mjs
@@ -4,7 +4,7 @@ const drawMetrics = project => {
 	const completed = project.filter(task => task.phaseid === 4).length;
 	const projectItems = project.length,
 		percentComplete = `${Math.round(parseFloat(completed / (projectItems || 1)) * 100)}%`;
-	return html('div', { className: 'metrics' }, [
+	return projectItems.length && html('div', { className: 'metrics' }, [
 		html('p', {}, html('strong', {}, ['Progress: ', `${completed}/${projectItems} ${percentComplete}`])),
 	])
 };
