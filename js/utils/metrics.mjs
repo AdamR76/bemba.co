88c20d839bf3c55574e9567607fc589a63057bb8
adam <adam@demandred.com>
2025-06-15 09:14:27 -0600

    don't NaN please

--- js/utils/metrics.mjs
+++ js/utils/metrics.mjs
@@ -3,7 +3,7 @@ import { html } from "../std.mjs";
 const drawMetrics = project => {
 	const completed = project.filter(task => task.phaseid === 4).length;
 	const projectItems = project.length,
-		percentComplete = `${Math.round(parseFloat(completed / projectItems) * 100)}%`;
+		percentComplete = `${Math.round(parseFloat(completed / (projectItems || 1)) * 100)}%`;
 	return html('div', { className: 'metrics' }, [
 		html('p', {}, html('strong', {}, ['Progress: ', `${completed}/${projectItems} ${percentComplete}`])),
 	])
